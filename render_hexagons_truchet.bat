ffa5f72f89ded1d65de257d9ce939d1ae5797337
Jeff Barr <jeff@vertexdev.com>
2025-12-18 20:06:14 +0100

    Windows batch file to render all layers of all mats

---
+++ render_hexagons_truchet.bat
@@ -0,0 +1,53 @@
+@ECHO OFF
+REM
+REM render_hexagons_truchet.bat
+REM
+REM For each of the parts (A-M) of the Truchet Hexagon mat pieces, render all 4 or 5 layers
+REM
+REM Run this 2 levels inside of Dropbox\3D Printing\My Models\FabricPrinting\STLs:
+REM
+REM 1 - Go to that directory and create a new directory within it named for the mat
+REM
+REM 2 - Enter that directory
+REM
+REM 3 - Create a params file in FabricPrinting (PARAM below) with a named param set (PMAT)
+REM
+REM 4 - Run this script and check output for errors
+REM
+REM 5 - Load each mat into PrusaSlicer (%OUTFILE%_%%M), keep as objects, slice, 
+REM     save as GCODE
+REM
+REM This generates up to 65 files and over 1.3 GB of STL within minutes
+REM
+REM More info:
+REM  https://docs.google.com/document/d/1lFDn3-urD4o3PvHtm6bd0SUD1x2gzSSJPIn_AOtbqk4/edit?tab=t.0
+
+REM Path to OpenSCAD, latest nightly build since we need object()
+set OSC="C:\Program Files\OpenSCAD (Nightly)\openscad.exe"
+
+REM Extra arguments to OpenSCAD, enable all extensions including object()
+set EXTRA=--enable all
+
+REM File to render
+set INFILE=..\..\hexagons_truchet.scad
+
+REM Param file to use (created in OpenSCAD)
+set PARAM=..\..\mat5.json
+
+REM Settings in param file
+SET PMAT=Mat5
+
+REM Base name for output files
+set OUTFILE=hexagons_truchet_mat5
+
+echo %OSC%
+echo %BASE%
+
+for %%M in (A B C D E F G H I J K L M) do (
+  echo MAT: %%M
+  for %%E in (1 2 3 4 5) do (
+    echo   EXT: %%E
+    %OSC% %EXTRA% -D "_Mat=\"%%M\"" -D "_WhichExtruder=%%E" -p %PARAM% -P %PMAT% %INFILE% -o %OUTFILE%_%%M_%%E.stl
+  )
+)
+
